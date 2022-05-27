8306a691264b93c13bd3e645d9d0d6d60e9dd4e3
Davi <davi.freitas@useall.com.br>
2022-05-27 13:46:58 -0300

    first commit

---
+++ app/routes/jokes/$jokeId.tsx
@@ -0,0 +1,14 @@
+export default function JokeRoute() {
+    return (
+        <div>
+            <p>
+                Here's your hilarious joke:
+            </p>
+            <p>
+                Why don't you find hippopotamouses hiding in trees?
+                    They're really good at it.
+                `
+            </p>
+        </div>
+    )
+}
